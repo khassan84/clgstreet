e8462e45e0f3665cfd05ed76d9464d04fee6d4e1
khassan84 <kamalhassan.bme@gmail.com>
2019-11-03 02:08:20 +0530

    commit bookstore db connect

---
+++ BookstoreDBConnect/src/com/bookstore/bean/BaseDO.java
@@ -0,0 +1,5 @@
+package com.bookstore.bean;
+
+public class BaseDO {
+
+}
---
+++ BookstoreDBConnect/src/com/bookstore/bean/UserPasswordDO.java
@@ -0,0 +1,10 @@
+package com.bookstore.bean;
+
+public class UserPasswordDO {
+	
+	private Long id;
+	private String userName;
+	private String createDate;
+	private String modifiedDate;
+
+}
